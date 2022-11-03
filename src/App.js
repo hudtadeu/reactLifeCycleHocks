3fe2c72c78cc02e3e873e165f62188cb0c93a35e
Hudson Tadeu <hudinvesting@gmail.com>
2022-11-03 19:00:20 -0300

    Convertendo componente Users para Hocks

--- src/App.js
+++ src/App.js
@@ -1,46 +1,37 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import Toggle from './components/Toggle';
 import Users from './components/Users';
 
-export default class App extends Component {
-  constructor() {
-    super();
+export default function App() {
+  const [users, setUsers] = useState([]);
+  const [showUsers, setShowUsers] = useState(false);
 
-    this.state = {
-      users: [],
-      showUsers: false,
-    }
-  }
-  async componentDidMount() {
-    const res = await fetch(
-      'https://randomuser.me/api/?seed=rush&nst=br&results=10'
-    );
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const res = await fetch(
+        'https://randomuser.me/api/?seed=rush&nst=br&results=10'
+      );
+      const json = await res.json();
+      setUsers(json.results);
+    };
 
-    const json = await res.json();
+    fetchUsers();
+  }, []);
 
-    this.setState({
-      users: json.results,
-    });
-  }
-
-  handleShowUsers = (isChecked) => {
-    this.setState({ showUsers: isChecked });
+  const handleShowUsers = (isChecked) => {
+    setShowUsers(isChecked);
   };
 
-  render() {
-    const { showUsers, users } = this.state;
-
-    return (
-      <div>
-        <h3>React LifeCycle</h3>
-        <Toggle
-          description="Mostrar usuários:"
-          enabled={showUsers}
-          onToggle={this.handleShowUsers}
-        />
-        <hr />
-        {showUsers && <Users users={users} />}
-      </div>
-    )
-  }
+  return (
+    <div>
+      <h3>React LifeCycle</h3>
+      <Toggle
+        description="Mostrar usuários:"
+        enabled={showUsers}
+        onToggle={handleShowUsers}
+      />
+      <hr />
+      {showUsers && <Users users={users} />}
+    </div>
+  )
 }
