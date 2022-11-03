ba48bcac248b5a420688cb70eaffdb4debb38d1d
Hudson Tadeu <hudinvesting@gmail.com>
2022-11-03 18:43:14 -0300

    Convertendo componente App para Hocks

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,53 +1,31 @@
-import React, { Component } from 'react'
+import React, { useEffect, useState } from 'react'
 import User from './User';
 
-export default class Users extends Component {
-  constructor() {
-    super();
+export default function Users({ users }) {
+  const [secondsVisible, setSecondsVisible] = useState(0);
 
-    this.state = {
-      secondsVisible: 0,
-    };
-
-    this.interval = null;
-  }
-
-  componentDidMount() {
-    console.log('componentDidMount de User.js');
-
-    this.interval = setInterval(() => {
-      const { secondsVisible } = this.state;
-
-      this.setState({
-        secondsVisible: secondsVisible + 1,
-      });
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setSecondsVisible(secondsVisible + 1);
     }, 1000);
-  }
-  componentDidUpdate() {
-    console.log('componentDidUpdate de User.js');
-  }
 
-  componentWillUnmount() {
-    clearInterval(this.interval);
-  }
+    return () => {
+      clearInterval(interval);
+    };
+  }, [secondsVisible]);
 
-  render() {
-    const { users } = this.props;
-    const { secondsVisible } = this.state;
-
-    return (
-      <div>
-        <p>Componente Users visível por {secondsVisible} segundos</p>
-        <ul>
-          {users.map((user) => {
-            const { login, name, picture } = user;
-            return (<li key={login.uuid}>
-              <User user={user} />
-            </li>
-            );
-          })}
-        </ul>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <p>Componente Users visível por {secondsVisible} segundos</p>
+      <ul>
+        {users.map((user) => {
+          const { login } = user;
+          return (<li key={login.uuid}>
+            <User user={user} />
+          </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
